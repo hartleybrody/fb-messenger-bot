ec4d3ad4d40a8c36d5b914d5380ea2ca94fc10b4
Hartley Brody <hartley.brody@gmail.com>
2016-06-15 14:01:12 -0400

    let's try parsing out all the parts of the message and responding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-import sys
+import json
 
+import requests
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -16,7 +17,52 @@ def webook():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"]
 
+    data = request.get_json()
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for message in entry["messaging"]:
+
+                # the facebook ID of the person sending you the message
+                sender_id = message["sender"]["id"]
+
+                # the recipient's ID, which should be your page's facebook ID
+                recipient_id = message["recipient"]["id"]
+
+                # the timestamp of the message
+                timestamp = message["timestamp"]
+
+                # the message ID
+                message_id = message["message"]["mid"]
+
+                # the message sequence number
+                message_seq = message["message"]["seq"]
+
+                # the message text
+                message_text = message["message"]["text"]
+
+                send_message(recipient_id, "got it, thanks!")
+
     return "ok"
+
+
+def send_message(recipient_id, message_text):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
 if __name__ == '__main__':
