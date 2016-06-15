83dc36aa8ed4ce457d53b95993eebef877689370
Hartley Brody <hartley.brody@gmail.com>
2016-06-15 12:36:02 -0400

    switch from ping/pong to hello world

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/ping/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def faq():
-    return "PONG"
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True)
