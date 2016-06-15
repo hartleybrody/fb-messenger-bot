bf270c1109c315f1158935a4fd082d00c043d9f9
Hartley Brody <hartley.brody@gmail.com>
2016-06-15 12:41:44 -0400

    make webhook echo back the challenge value

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def faq():
+
+    # when endpoint is registered as a webhook, it must
+    # return the 'hub.challenge' value in the query arguments
+    if request.args.get("hub.challenge"):
+        return request.args["hub.challenge"]
+
     return "Hello World"
 
 if __name__ == '__main__':
