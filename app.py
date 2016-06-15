77018489df131ea959943d34a1c140ce56db1c3e
Hartley Brody <hartley.brody@gmail.com>
2016-06-15 13:09:43 -0400

    add verification token checking

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+import os
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def faq():
+def webook():
+
+    if not request.args.get("verify_token") == os.environ["VERIFY_TOKEN"]:
+        return "Verification token mismatch", 403
 
     # when endpoint is registered as a webhook, it must
     # return the 'hub.challenge' value in the query arguments
-    if request.args.get("hub.challenge"):
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         return request.args["hub.challenge"]
 
-    return "Hello World"
+    return "ok"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
