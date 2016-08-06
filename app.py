f210a1c1539e0dbe8e4bfdf6ae101e4617a39e30
gauravlakhanidev <gaurav.lakhani@gmail.com>
2016-08-06 21:46:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def webook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "got it, thanks!")
+                    send_message(sender_id, " Thanks for reaching to Fastfox, Please share your details here. 
+                    We will revert as soon as possible. Many Thanks!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
