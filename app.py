3e1933b31c7db7b543a3b55f300aabcea152a568
Hartley Brody <hartley.brody@gmail.com>
2017-03-02 14:13:31 +0700

    update welcome message

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "got it, thanks!")
+                    send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
