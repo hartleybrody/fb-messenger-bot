eb3b3abab58332417b7fde47b267fbdded09c1b8
Junwon Park <junwonpk@stanford.edu>
2017-03-25 02:13:42 -0700

    redis test

--- kova.py
+++ kova.py
@@ -1,6 +1,6 @@
 import os
 import redis
-import pickle
+import cPickle
 
 class Kova:
 
@@ -8,8 +8,10 @@ class Kova:
         self.redis = redis.from_url(os.environ.get("REDISCLOUD_URL"))
 
     def chat(self, input, user_id):
-        self.redis.hsetnx('users', user_id + 'chapter', int(0))
-        chapter = int(self.redis.hget('users', user_id + 'chapter'))
-        chapter += 1
-        self.redis.hset('users', userid + 'chapter', int(chapter))
-        return 'you messaged + ' + chapter + ' times'
+        if user_id not in self.redis.keys():
+            userdata = {'count': 0}
+            self.redis.set(user_id, cPickle.dumps(userdata))
+        userdata = cPickle.load(self.redis.get(user_id))
+        userdata['count'] += 1
+        self.redis.set(user_id cPickle.dumps(userdata))
+        return 'you messaged + ' + userdata['count'] + ' times'
