5ff809b1fc896ede5dcca2f00cf047f5ff4583ad
Junwon Park <junwonpk@stanford.edu>
2017-03-25 02:18:13 -0700

    redis test

--- kova.py
+++ kova.py
@@ -13,5 +13,5 @@ class Kova:
             self.redis.set(user_id, cPickle.dumps(userdata))
         userdata = cPickle.load(self.redis.get(user_id))
         userdata['count'] += 1
-        self.redis.set(user_id cPickle.dumps(userdata))
+        self.redis.set(user_id, cPickle.dumps(userdata))
         return 'you messaged + ' + userdata['count'] + ' times'
