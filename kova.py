982e781a7c0d10cee2b207ac4f36c5119b97bb9d
Junwon Park <junwonpk@stanford.edu>
2017-03-25 02:59:58 -0700

    Redis implemented for storing contextual information.

--- kova.py
+++ kova.py
@@ -18,11 +18,11 @@ class Kova:
         return 'you messaged ' + str(user_data['count']) + ' times'
 
     def initUser(self, user_id):
-        userdata = {'count': 0}
-        self.redis.set(user_id, cPickle.dumps(userdata))
+        user_data = {'count': 0}
+        self.redis.set(user_id, cPickle.dumps(user_data))
 
     def getData(self, user_id):
         return cPickle.loads(self.redis.get(user_id))
 
     def setData(self, user_id, user_data):
-        self.redis.set(user_id, cPickle.dumps(userdata))
+        self.redis.set(user_id, cPickle.dumps(user_data))
