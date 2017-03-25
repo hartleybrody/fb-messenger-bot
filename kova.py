6f8cdf504975ea1b2668b1568442f189a1ba5721
Junwon Park <junwonpk@stanford.edu>
2017-03-25 02:51:34 -0700

    redis build

--- kova.py
+++ kova.py
@@ -8,14 +8,19 @@ class Kova:
         self.redis = redis.from_url(os.environ.get("REDISCLOUD_URL"))
 
     def chat(self, input, user_id):
-        print 'redis keys '
-        print self.redis.keys()
-        if user_id not in self.redis.keys():
-            userdata = {'count': 0}
-            print 'checkpoint'
-            print user_id
-            self.redis.set(user_id, cPickle.dumps(userdata))
-        userdata = cPickle.loads(self.redis.get(user_id))
-        userdata['count'] += 1
+        if user_id not in self.redis.keys(): # if user first time talking
+            initUser(user_id)
+        user_data = self.getData(user_id)
+        user_data['count'] += 1
+        self.setData(user_id, user_data)
+        return 'you messaged ' + str(user_data['count']) + ' times'
+
+    def initUser(self, user_id):
+        userdata = {'count': 0}
         self.redis.set(user_id, cPickle.dumps(userdata))
-        return 'you messaged + ' + str(userdata['count']) + ' times'
+
+    def getData(self, user_id):
+        return cPickle.loads(self.redis.get(user_id))
+
+    def setData(self, user_id, user_data):
+        self.redis.set(user_id, cPickle.dumps(userdata))
