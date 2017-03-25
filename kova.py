3b77422e3d97d011529738f20b9d04ea07599215
Junwon Park <junwonpk@stanford.edu>
2017-03-24 23:34:37 -0700

    redis test

--- kova.py
+++ kova.py
@@ -1,8 +1,14 @@
+import os
+from urlparse import urlparse
+import redis
+
 class Kova:
 
     def __init__(self):
-      self.chapter = 0
+        self.redisurl = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+        self.redis = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+        self.redis.set('test', 'test is success')
 
     def chat(self, input):
         self.chapter += 1
-        return "I see that you said " + str(self.chapter) + ' times'
+        return self.redis.get('test')
