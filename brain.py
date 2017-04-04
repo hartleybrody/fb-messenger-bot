7743d72b60618d643950464601145019c0258a2f
Hartley Brody <hartley.brody@gmail.com>
2017-04-03 18:27:44 -0700

    change the bot to respond w a google maps link to cafes

--- brain.py
+++ brain.py
@@ -7,13 +7,14 @@ def process_message(message_payload, message_type):
 
     if message_payload.lower() in greetings:
         responses.append("Hello there!")
-        responses.append(dict(text="In order to better assist you, we'll need your location", get_location=True))
+        responses.append(dict(text="In order to find nearby cafes, we'll need your location", get_location=True))
 
     if message_type == "location":
         coordinates = json.loads(message_payload)["coordinates"]
         lat = coordinates["lat"]
         lon = coordinates["long"]
-        responses.append("You are located at {}, {}".format(lat, lon))
+        responses.append("Here's a link with a map to nearby cafes:")
+        responses.append("https://www.google.com/maps/search/cafe/@{},{},15z/data=!3m1!4b1".format(lat, lon))
 
 
     return responses
