26ae4055d576e5f6571ea093b80f56f4a03c81d9
Oren Fisk <oren.fisk@bazaarvoice.com>
2017-05-10 13:20:37 -0500

    Formatting candy request message; placeholder for requester name

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ def webhook():
                                 Items = [candyDb]
                             )
                             log("Posting to bother Oren")
-                            r = requests.post("https://iimhlox1ml.execute-api.us-east-1.amazonaws.com/hackathon/candy-request?requestId=gibberish", data=json.dumps(data))
+                            candy_request = {"senderId": sender_id, "choice": message_text, "name": "TODO"}
+                            r = requests.post("https://iimhlox1ml.execute-api.us-east-1.amazonaws.com/hackathon/candy-request?requestId=gibberish", data=json.dumps(candy_request))
                             log(r)
 
                     #log(bot.get_user_info(sender_id))
