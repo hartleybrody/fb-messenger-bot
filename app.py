07f93e8731db8095bf1de38aa830619ba0632973
Jonathan Munoz Gomez <jonathanmunozgomez@gmail.com>
2017-10-05 09:33:15 -0500

    Improving string cast for log

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def send_message(recipient_id, message_text):
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+    print message.encode('utf-8').strip()
     sys.stdout.flush()
 
 
